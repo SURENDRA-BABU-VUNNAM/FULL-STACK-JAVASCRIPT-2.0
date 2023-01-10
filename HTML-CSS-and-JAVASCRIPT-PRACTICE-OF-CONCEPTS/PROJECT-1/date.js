1e11800a203f6ae29989b6acf4bf1ac7aeb6d0c4
surendrababuvunnam <vunnam.surendrababu@gmail.com>
2023-01-10 06:52:44 +0530

    date and methods practice

--- HTML-CSS-and-JAVASCRIPT-PRACTICE-OF-CONCEPTS/PROJECT-1/date.js
+++ HTML-CSS-and-JAVASCRIPT-PRACTICE-OF-CONCEPTS/PROJECT-1/date.js
@@ -1,8 +1,15 @@
-const now = new Date();
-console.log(now.toString());
+// console.log(Date());
 
-console.log(now.getFullYear());
-console.log(now.getMonth());
-console.log(now.getDay());
+// milli-seconds
+// console.log(new Date(10000000000))
 
+// date-string
+// console.log(new Date("2015-03-25"));
+
+// date object
+// console.log(new Date(2023, 1, 10, 16, 33, 30, 0));
+
+// getDate() method
+// const d = new Date();
+// console.log(d.getDate());
 
